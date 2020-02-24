cf77fefa3bc8afc40158881fcede7a00ef4a9b82
Ian <mus5002@aol.com>
2020-02-24 17:31:38 -0500

    added Hand component and refactored Computer and Player component to remove duplicate code

--- src/components/Computer.js
+++ src/components/Computer.js
@@ -2,10 +2,11 @@ import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { actionCreators } from '../actions/actions'
+import Hand from './Hand'
 import {
     Image,
     Back,
-    Hand
+    HandDiv
 } from '../assets/styles'
 
 import card from '../assets/playing-card-back.jpg'
@@ -19,23 +20,13 @@ const Computer = ({ deck, computer, player, actions, winner }) => {
 
     if (!computer.hand[0]) return <h1>Loading...</h1>
 
-    if (player.stand || winner) return (
-        <Hand>
-            {
-                computer.hand.map((card, indx) => {
-                    return <Image
-                            key={indx}
-                            src={card.image} />
-                })
-            }
-        </Hand>
-    )
+    if (player.stand || winner) return <Hand char={computer} />
 
     return (
-        <Hand>
+        <HandDiv>
             <Image src={computer.hand[0].image} />
             <Back src={card} alt='card face down' />
-        </Hand>
+        </HandDiv>
     )
 }
 
